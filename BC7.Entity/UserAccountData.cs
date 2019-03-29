2e395298de88d4dfda5b279d910d5052dca0cfae
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-29 19:20:31 +0100

    Added UserAccountData entity class

---
+++ BC7.Entity/UserAccountData.cs
@@ -0,0 +1,18 @@
+﻿using BC7.Entity.AbstractBase;
+
+namespace BC7.Entity
+{
+    public class UserAccountData : BaseEntity
+    {
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Salt { get; set; }
+        public string Hash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+    }
+}
