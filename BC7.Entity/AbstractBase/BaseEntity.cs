33e9a5c4e1a3e6ce6d98c8a031bc62244f979d1e
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-29 19:12:21 +0100

    Created BaseEntity with ID and CreatedAt properties

---
+++ BC7.Entity/AbstractBase/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BC7.Entity.AbstractBase
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
