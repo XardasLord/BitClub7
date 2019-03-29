19d3a688b2e0621756a6702539f8a2fa0ee5d489
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-29 18:11:16 +0100

    Added BitClub7Context and created an interface for it to be injected later in IOC

---
+++ BC7.Database/BitClub7Context.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BC7.Database
+{
+    public class BitClub7Context : DbContext, IBitClub7Context
+    {
+        public BitClub7Context()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // TODO: Auto inserting dates?
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ BC7.Database/IBitClub7Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BC7.Database
+{
+    public interface IBitClub7Context : IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
