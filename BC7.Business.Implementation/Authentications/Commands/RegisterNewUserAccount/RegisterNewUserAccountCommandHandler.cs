dff9977257680be2ca26fa5055e273c30686ad5f
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 10:26:11 +0100

    Injected context into a handler

--- BC7.Business.Implementation/Authentications/Commands/RegisterNewUserAccount/RegisterNewUserAccountCommandHandler.cs
+++ BC7.Business.Implementation/Authentications/Commands/RegisterNewUserAccount/RegisterNewUserAccountCommandHandler.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using BC7.Database;
 using MediatR;
 
 namespace BC7.Business.Implementation.Authentications.Commands.RegisterNewUserAccount
 {
     public class RegisterNewUserAccountCommandHandler : IRequestHandler<RegisterNewUserAccountCommand, Guid>
     {
-        public RegisterNewUserAccountCommandHandler()
+        private readonly IBitClub7Context _context;
+
+        public RegisterNewUserAccountCommandHandler(IBitClub7Context context)
         {
+            _context = context;
         }
 
         public Task<Guid> Handle(RegisterNewUserAccountCommand request, CancellationToken cancellationToken)
