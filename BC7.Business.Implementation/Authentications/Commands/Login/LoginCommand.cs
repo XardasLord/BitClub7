495d803714610792ab0e2d73d882714df8e2f276
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 19:34:25 +0100

    Implemented Login command handler logic

---
+++ BC7.Business.Implementation/Authentications/Commands/Login/LoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BC7.Business.Implementation.Authentications.Commands.Login
+{
+    public class LoginCommand : IRequest<string>
+    {
+        public string LoginOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
