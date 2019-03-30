eda7af1db4314bae61ba55205cd61b23547b8292
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 08:54:42 +0100

    Added MatrixPosition entity

---
+++ BC7.Entity/MatrixPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BC7.Entity.AbstractBase;
+
+namespace BC7.Entity
+{
+    public class MatrixPosition : BaseEntity
+    {
+        public Guid UserMultiAccountId { get; set; }
+        public virtual UserMultiAccount UserMultiAccount { get; set; }
+        public Guid? ParentId { get; set; }
+        public virtual MatrixPosition ParentMatrixPosition { get; set; }
+
+        public int MatrixLevel { get; set; }
+        public int Left { get; set; }
+        public int Right { get; set; }
+        public int DepthLevel { get; set; }
+    }
+}
