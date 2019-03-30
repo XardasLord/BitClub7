6c1163976004a89e151f3936e8463623040b4c62
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 19:09:32 +0100

    Added TODO comment to change in the future

--- BC7.Business.Implementation/Authentications/Commands/RegisterNewUserAccount/RegisterNewUserAccountCommandHandler.cs
+++ BC7.Business.Implementation/Authentications/Commands/RegisterNewUserAccount/RegisterNewUserAccountCommandHandler.cs
@@ -53,7 +53,7 @@ namespace BC7.Business.Implementation.Authentications.Commands.RegisterNewUserAc
                 UserAccountDataId = userAccountData.Id,
                 UserMultiAccountInvitingId = invitingUserMultiAccountId,
                 MultiAccountName = userAccountData.Login,
-                RefLink = _reflinkHelper.GenerateReflink()
+                RefLink = _reflinkHelper.GenerateReflink() // TODO: This should be empty on first account creating - it should be generated after buying the lvl0 matrix position
             };
             await _context.Set<UserMultiAccount>().AddAsync(userMultiAccount);
             await _context.SaveChangesAsync();
