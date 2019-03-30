3287d50b668cbb16448d89da22df1c626f2ef019
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 19:55:57 +0100

    Created LoginModel instead of passing command from frontend to API

---
+++ BC7.Business/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BC7.Business.Models
+{
+    public class LoginModel
+    {
+        public string LoginOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
