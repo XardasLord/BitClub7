0ad08b70b9861de1d240546d254e0001fd93c14c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 07:18:50 +0100

    Added a colleration to User in multiaccount which invited by his reflink another user

--- BC7.Entity/UserMultiAccount.cs
+++ BC7.Entity/UserMultiAccount.cs
@@ -7,6 +7,10 @@ namespace BC7.Entity
     {
         public Guid UserAccountDataId { get; set; }
         public virtual UserAccountData UserAccountData { get; set; }
+
+        public virtual Guid? InvitedByUserMultiAccountId { get; set; }
+        public virtual UserMultiAccount UserMultiAccountInviting { get; set; }
+
         public int AccountNumber { get; set; }
         public string RefLink { get; set; }
     }
