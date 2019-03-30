aff20918850fe4dd74cee015fe09b26e28ff5ad8
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 14:07:46 +0100

    Added predefined seed data (3 roots accounts)

---
+++ BC7.Database/Migrations/20190330125259_Renamed_Column_To_MultiAccountName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BC7.Database.Migrations
+{
+    public partial class Renamed_Column_To_MultiAccountName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "UserMultiAccounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MultiAccountName",
+                table: "UserMultiAccounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MultiAccountName",
+                table: "UserMultiAccounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountNumber",
+                table: "UserMultiAccounts",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
