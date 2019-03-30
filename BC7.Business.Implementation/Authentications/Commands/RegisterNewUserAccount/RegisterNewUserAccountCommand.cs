edf0eaf9064d089e4eeacff0e52941f065051395
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 10:25:09 +0100

    Created RegisterNewUserAccountCommand and its empty handler for now

---
+++ BC7.Business.Implementation/Authentications/Commands/RegisterNewUserAccount/RegisterNewUserAccountCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MediatR;
+
+namespace BC7.Business.Implementation.Authentications.Commands.RegisterNewUserAccount
+{
+    public class RegisterNewUserAccountCommand : IRequest<Guid>
+    {
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string BtcWalletAddress { get; set; }
+    }
+}
---
+++ BC7.Business.Implementation/Authentications/Commands/RegisterNewUserAccount/RegisterNewUserAccountCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BC7.Business.Implementation.Authentications.Commands.RegisterNewUserAccount
+{
+    public class RegisterNewUserAccountCommandHandler : IRequestHandler<RegisterNewUserAccountCommand, Guid>
+    {
+        public RegisterNewUserAccountCommandHandler()
+        {
+        }
+
+        public Task<Guid> Handle(RegisterNewUserAccountCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
