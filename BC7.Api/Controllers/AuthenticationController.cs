a5313a6cdfea6676ee0914dc87087eff77d943cc
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-30 10:18:10 +0100

    Created an empty Authentication controller with mediator injected in

---
+++ BC7.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BC7.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthenticationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
