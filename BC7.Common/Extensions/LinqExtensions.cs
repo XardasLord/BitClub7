9b7e2d2ad400fffbb22cccc6c6a12712cb786591
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-31 12:06:15 +0200

    Created new project BC7.Common with commot utilities which will be shared over many projects

---
+++ BC7.Common/Extensions/LinqExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BC7.Common.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static bool ContainsAll<T>(this IEnumerable<T> containingList, IEnumerable<T> lookupList)
+        {
+            return !lookupList.Except(containingList).Any();
+        }
+    }
+}
