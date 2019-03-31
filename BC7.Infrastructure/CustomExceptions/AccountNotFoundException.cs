c76f2c40b0f1010079d7a4086ada8c65d90cda09
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-31 15:44:49 +0200

    Created custom exception type: AccountNotFoundException

---
+++ BC7.Infrastructure/CustomExceptions/AccountNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BC7.Infrastructure.CustomExceptions
+{
+    public class AccountNotFoundException : Exception
+    {
+        public AccountNotFoundException()
+        {
+        }
+
+        public AccountNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public AccountNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
