a9a4eba0441ba27df69468e5ca816d055307d7e1
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-31 13:20:05 +0200

    Added request + handler to get multiAccounts for the given user

---
+++ BC7.Business.Implementation/Users/Requests/GetMultiAccounts/GetMultiAccountsRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BC7.Business.Models;
+using MediatR;
+
+namespace BC7.Business.Implementation.Users.Requests.GetMultiAccounts
+{
+    public class GetMultiAccountsRequest : IRequest<UserMultiAccountModel>
+    {
+        public Guid UserAccountId { get; set; }
+    }
+}
---
+++ BC7.Business.Implementation/Users/Requests/GetMultiAccounts/GetMultiAccountsRequestHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BC7.Business.Models;
+using MediatR;
+
+namespace BC7.Business.Implementation.Users.Requests.GetMultiAccounts
+{
+    public class GetMultiAccountsRequestHandler : IRequestHandler<GetMultiAccountsRequest, UserMultiAccountModel>
+    {
+        public Task<UserMultiAccountModel> Handle(GetMultiAccountsRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
