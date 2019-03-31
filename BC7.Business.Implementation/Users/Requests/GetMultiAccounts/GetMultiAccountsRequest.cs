ef43a6285d68303a0eda1fed9cbcd184d09bafc4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-03-31 14:07:51 +0200

    Implemented handler logic of getting multi accounts for specific user

--- BC7.Business.Implementation/Users/Requests/GetMultiAccounts/GetMultiAccountsRequest.cs
+++ BC7.Business.Implementation/Users/Requests/GetMultiAccounts/GetMultiAccountsRequest.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using BC7.Business.Models;
 using MediatR;
 
 namespace BC7.Business.Implementation.Users.Requests.GetMultiAccounts
 {
-    public class GetMultiAccountsRequest : IRequest<UserMultiAccountModel>
+    public class GetMultiAccountsRequest : IRequest<IEnumerable<UserMultiAccountModel>>
     {
         public Guid UserAccountId { get; set; }
     }
