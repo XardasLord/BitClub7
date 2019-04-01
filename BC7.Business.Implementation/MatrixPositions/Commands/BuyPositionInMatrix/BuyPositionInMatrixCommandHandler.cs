f4f9773463bfd2e798cf457b3685c082f26e2cd9
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-01 15:53:41 +0200

    Created an empty handler responsible for buying position in matrix

---
+++ BC7.Business.Implementation/MatrixPositions/Commands/BuyPositionInMatrix/BuyPositionInMatrixCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BC7.Business.Implementation.MatrixPositions.Commands.BuyPositionInMatrix
+{
+    public class BuyPositionInMatrixCommandHandler : IRequestHandler<BuyPositionInMatrixCommand, Guid>
+    {
+        public Task<Guid> Handle(BuyPositionInMatrixCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
