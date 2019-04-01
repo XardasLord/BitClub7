78832e24c36cf4970656cf7633d0b44d61d46487
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-01 19:11:29 +0200

    Injected a context into a handler's constructor

--- BC7.Business.Implementation/MatrixPositions/Commands/BuyPositionInMatrix/BuyPositionInMatrixCommandHandler.cs
+++ BC7.Business.Implementation/MatrixPositions/Commands/BuyPositionInMatrix/BuyPositionInMatrixCommandHandler.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using BC7.Database;
 using MediatR;
 
 namespace BC7.Business.Implementation.MatrixPositions.Commands.BuyPositionInMatrix
 {
     public class BuyPositionInMatrixCommandHandler : IRequestHandler<BuyPositionInMatrixCommand, Guid>
     {
+        private readonly IBitClub7Context _context;
+
+        public BuyPositionInMatrixCommandHandler(IBitClub7Context context)
+        {
+            _context = context;
+        }
+
         public Task<Guid> Handle(BuyPositionInMatrixCommand request, CancellationToken cancellationToken)
         {
+
             throw new NotImplementedException();
         }
     }
