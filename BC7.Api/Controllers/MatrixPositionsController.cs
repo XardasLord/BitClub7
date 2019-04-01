fba5dfc297c29f6e8a2776d540105beb6f5b3d4f
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-01 15:29:33 +0200

    Created MatrixPositions controller

---
+++ BC7.Api/Controllers/MatrixPositionsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BC7.Api.Controllers
+{
+    public class MatrixPositionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MatrixPositionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
+
+// TODO: MatrixPositionsController -> api/matrixPositions/{userMultiAccountId} (query: ?matrixLevel=0) [POST]
