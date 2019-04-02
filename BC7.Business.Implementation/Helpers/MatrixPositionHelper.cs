643a635019ab7e0b25a619a2415a5d6724df1b55
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-02 19:49:18 +0200

    Added a matrix position helper (empty) and registered it into an IOC container in API project

---
+++ BC7.Business.Implementation/Helpers/MatrixPositionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BC7.Business.Helpers;
+using BC7.Entity;
+
+namespace BC7.Business.Implementation.Helpers
+{
+    public class MatrixPositionHelper : IMatrixPositionHelper
+    {
+        public MatrixPositionHelper()
+        {
+        }
+
+        public Task<IEnumerable<MatrixPosition>> GetMatrixForUserMultiAccount(Guid userMultiAccountId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BC7.Business/Helpers/IMatrixPositionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BC7.Entity;
+
+namespace BC7.Business.Helpers
+{
+    public interface IMatrixPositionHelper
+    {
+        Task<IEnumerable<MatrixPosition>> GetMatrixForUserMultiAccount(Guid userMultiAccountId);
+    }
+}
