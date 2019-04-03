c2d4dd4c331a6f3594d4c0ecc0c959269cbc609d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-03 20:36:32 +0200

    Created a MatrixPositionBought event + not implemented handler

---
+++ BC7.Business.Implementation/Events/MatrixPositionBoughtEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BC7.Business.Implementation.Events
+{
+    public class MatrixPositionBoughtEventHandler : INotificationHandler<MatrixPositionBoughtEvent>
+    {
+        public Task Handle(MatrixPositionBoughtEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
