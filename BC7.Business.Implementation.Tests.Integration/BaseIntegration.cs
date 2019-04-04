3c4d8e01c9d7e9af9b6711dd16464be6b82bbc5e
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-04 16:58:52 +0200

    Moved the whole bae funtionality (creating test DB + deleting after each test) to the BaseIntegration abstract class

---
+++ BC7.Business.Implementation.Tests.Integration/BaseIntegration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using BC7.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace BC7.Business.Implementation.Tests.Integration
+{
+    public class BaseIntegration
+    {
+        protected IBitClub7Context _context;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkSqlServer()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<BitClub7Context>();
+            builder.UseSqlServer($@"Server=XARDASLORD\SQLEXPRESS;Database=BitClub7_integration_tests_{Guid.NewGuid()};Integrated Security=SSPI")
+                .UseInternalServiceProvider(serviceProvider);
+
+            _context = new BitClub7Context(builder.Options);
+            _context.Database.Migrate();
+
+            await ClearDatabase();
+        }
+
+        public async Task ClearDatabase()
+        {
+            var matrices = _context.MatrixPositions;
+            var multiAccounts = _context.UserMultiAccounts;
+            var users = _context.UserAccountsData;
+
+            _context.MatrixPositions.RemoveRange(matrices);
+            _context.UserMultiAccounts.RemoveRange(multiAccounts);
+            _context.UserAccountsData.RemoveRange(users);
+
+            await _context.SaveChangesAsync();
+        }
+
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _context.Database.EnsureDeletedAsync();
+        }
+    }
+}
--- BC7.Business.Implementation.Tests.Integration/RegisterNewUserAccountTests.cs
+++ BC7.Business.Implementation.Tests.Integration/RegisterNewUserAccountTests.cs
@@ -1,52 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
-using BC7.Database;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace BC7.Business.Implementation.Tests.Integration
 {
     [TestFixture]
-    public class RegisterNewUserAccountTests
+    public class RegisterNewUserAccountTests : BaseIntegration
     {
-        private IBitClub7Context _context;
-
-        [SetUp]
-        public async Task SetUp()
+        [Test]
+        public void RegisterNewUserAccountCommandHandler_WhenHandle_CreateUserAccountAndItsMultiAccount()
         {
-            var serviceProvider = new ServiceCollection()
-                .AddEntityFrameworkSqlServer()
-                .BuildServiceProvider();
-
-            var builder = new DbContextOptionsBuilder<BitClub7Context>();
-            builder.UseSqlServer($@"Server=XARDASLORD\SQLEXPRESS;Database=BitClub7_integration_tests_{Guid.NewGuid()};Integrated Security=SSPI")
-                .UseInternalServiceProvider(serviceProvider);
-
-            _context = new BitClub7Context(builder.Options);
-            _context.Database.Migrate();
-
-            await ClearDatabase();
-        }
-
-        public async Task ClearDatabase()
-        {
-            var matrices = _context.MatrixPositions;
-            var multiAccounts = _context.UserMultiAccounts;
-            var users = _context.UserAccountsData;
-
-            _context.MatrixPositions.RemoveRange(matrices);
-            _context.UserMultiAccounts.RemoveRange(multiAccounts);
-            _context.UserAccountsData.RemoveRange(users);
-
-            await _context.SaveChangesAsync();
-        }
-
-
-        [TearDown]
-        public async Task TearDown()
-        {
-            await _context.Database.EnsureDeletedAsync();
         }
     }
 }
