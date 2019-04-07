f9ef9c439125edb39aacd9d08f9fcc56eeb96e20
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-07 22:13:07 +0200

    Implemented the helper method which will find the nearest empty matrix position for the given

--- BC7.Business.Implementation/Helpers/MatrixPositionHelper.cs
+++ BC7.Business.Implementation/Helpers/MatrixPositionHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using BC7.Business.Helpers;
 using BC7.Entity;
 
@@ -20,6 +21,11 @@ namespace BC7.Business.Implementation.Helpers
         public bool CheckIfMatrixHasEmptySpace(IEnumerable<MatrixPosition> matrix)
         {
             return matrix.Any(x => x.UserMultiAccountId == null);
+        }
+
+        public Task<MatrixPosition> FindTheNearestEmptyPosition(Guid userMultiAccountId, int matrixLevel = 0)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- BC7.Business/Helpers/IMatrixPositionHelper.cs
+++ BC7.Business/Helpers/IMatrixPositionHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using BC7.Entity;
 
 namespace BC7.Business.Helpers
@@ -7,7 +8,7 @@ namespace BC7.Business.Helpers
     public interface IMatrixPositionHelper
     {
         bool CheckIfAnyAccountExistInMatrix(IEnumerable<MatrixPosition> matrix, IEnumerable<Guid> accountIds);
-
         bool CheckIfMatrixHasEmptySpace(IEnumerable<MatrixPosition> matrix);
+        Task<MatrixPosition> FindTheNearestEmptyPosition(Guid userMultiAccountId, int matrixLevel = 0);
     }
 }
