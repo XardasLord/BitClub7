12b7e27745cc9d1ebc55994c62164819321bad28
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-07 21:27:03 +0200

    Changed creating logic from helper to repository

--- BC7.Business.Implementation/Helpers/UserMultiAccountHelper.cs
+++ BC7.Business.Implementation/Helpers/UserMultiAccountHelper.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using BC7.Business.Helpers;
 using BC7.Database;
 using BC7.Entity;
-using BC7.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace BC7.Business.Implementation.Helpers
@@ -12,14 +11,10 @@ namespace BC7.Business.Implementation.Helpers
     public class UserMultiAccountHelper : IUserMultiAccountHelper
     {
         private readonly IBitClub7Context _context;
-        private readonly IReflinkHelper _reflinkHelper;
-        private readonly IUserMultiAccountRepository _userMultiAccountRepository;
 
-        public UserMultiAccountHelper(IBitClub7Context context, IReflinkHelper reflinkHelper, IUserMultiAccountRepository userMultiAccountRepository)
+        public UserMultiAccountHelper(IBitClub7Context context)
         {
             _context = context;
-            _reflinkHelper = reflinkHelper;
-            _userMultiAccountRepository = userMultiAccountRepository;
         }
 
         public Task<UserMultiAccount> GetRandomUserMultiAccount()
@@ -28,27 +23,6 @@ namespace BC7.Business.Implementation.Helpers
                 .OrderBy(r => Guid.NewGuid())
                 .Take(1)
                 .FirstAsync();
-        }
-
-#warning Move to repository
-        public async Task<UserMultiAccount> Create(Guid userAccountId, string reflink)
-        {
-            var userMultiAccountInviting = await _userMultiAccountRepository.GetByReflinkAsync(reflink);
-            var multiAccountName = await GenerateNextMultiAccountName(userAccountId);
-
-            var userMultiAccount = new UserMultiAccount
-            {
-                UserAccountDataId = userAccountId,
-                UserMultiAccountInvitingId = userMultiAccountInviting.Id,
-                MultiAccountName = multiAccountName,
-                RefLink = _reflinkHelper.GenerateReflink(),
-                IsMainAccount = false
-            };
-
-            await _context.Set<UserMultiAccount>().AddAsync(userMultiAccount);
-            await _context.SaveChangesAsync();
-
-            return userMultiAccount;
         }
 
         public async Task<string> GenerateNextMultiAccountName(Guid userAccountDataId)
