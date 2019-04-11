2808cd2061b93846fbeab5ea2fd5f29d34692e4c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-11 16:00:52 +0200

    Private set for all domain

--- BC7.Business.Implementation/Events/UserBoughtMatrixPositionEventHandler.cs
+++ BC7.Business.Implementation/Events/UserBoughtMatrixPositionEventHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using BC7.Business.Helpers;
-using BC7.Database;
 using BC7.Repository;
 using MediatR;
 
@@ -9,13 +8,11 @@ namespace BC7.Business.Implementation.Events
 {
     public class UserBoughtMatrixPositionEventHandler : INotificationHandler<UserBoughtMatrixPositionEvent>
     {
-        private readonly IBitClub7Context _context;
         private readonly IUserMultiAccountRepository _userMultiAccountRepository;
         private readonly IReflinkHelper _reflinkHelper;
 
-        public UserBoughtMatrixPositionEventHandler(IBitClub7Context context, IUserMultiAccountRepository userMultiAccountRepository, IReflinkHelper reflinkHelper)
+        public UserBoughtMatrixPositionEventHandler(IUserMultiAccountRepository userMultiAccountRepository, IReflinkHelper reflinkHelper)
         {
-            _context = context;
             _userMultiAccountRepository = userMultiAccountRepository;
             _reflinkHelper = reflinkHelper;
         }
@@ -23,10 +20,10 @@ namespace BC7.Business.Implementation.Events
         public async Task Handle(UserBoughtMatrixPositionEvent notification, CancellationToken cancellationToken = default(CancellationToken))
         {
             var multiAccount = await _userMultiAccountRepository.GetAsync(notification.MultiAccountId);
-            multiAccount.RefLink = _reflinkHelper.GenerateReflink();
 
-            // TODO: UpdateAsync in repository
-            await _context.SaveChangesAsync();
+            multiAccount.SetReflink(_reflinkHelper.GenerateReflink());
+
+            await _userMultiAccountRepository.UpdateAsync(multiAccount);
         }
     }
 }
