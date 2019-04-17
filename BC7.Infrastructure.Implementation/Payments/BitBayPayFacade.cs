78e967ffcdf0cfaa46f6d100c2da4cbd9ed66b57
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-17 16:33:42 +0200

    Injected the settings to the facade

--- BC7.Infrastructure.Implementation/Payments/BitBayPayFacade.cs
+++ BC7.Infrastructure.Implementation/Payments/BitBayPayFacade.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
 using BC7.Infrastructure.Payments;
+using BC7.Infrastructure.Payments.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace BC7.Infrastructure.Implementation.Payments
 {
     public class BitBayPayFacade : IBitBayPayFacade
     {
-        public BitBayPayFacade()
+        private readonly IOptions<BitBayPayApiSettings> _bitBayPayApiSettings;
+
+        public BitBayPayFacade(IOptions<BitBayPayApiSettings> bitBayPayApiSettings)
         {
+            _bitBayPayApiSettings = bitBayPayApiSettings;
         }
 
         public Task<string> CreatePayment()
