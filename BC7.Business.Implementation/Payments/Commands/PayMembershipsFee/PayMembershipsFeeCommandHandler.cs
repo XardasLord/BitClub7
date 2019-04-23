adc7695c515edadc6f739ebeebc397ddccd702fc
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-23 20:40:22 +0200

    Added command validation for BitBayPay payment handler

--- BC7.Business.Implementation/Payments/Commands/PayMembershipsFee/PayMembershipsFeeCommandHandler.cs
+++ BC7.Business.Implementation/Payments/Commands/PayMembershipsFee/PayMembershipsFeeCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using BC7.Infrastructure.CustomExceptions;
 using BC7.Infrastructure.Payments;
+using BC7.Repository;
 using MediatR;
 
 namespace BC7.Business.Implementation.Payments.Commands.PayMembershipsFee
@@ -9,23 +11,35 @@ namespace BC7.Business.Implementation.Payments.Commands.PayMembershipsFee
     public class PayMembershipsFeeCommandHandler : IRequestHandler<PayMembershipsFeeCommand>
     {
         private readonly IBitBayPayFacade _bitBayPayFacade;
+        private readonly IUserAccountDataRepository _userAccountDataRepository;
 
-        public PayMembershipsFeeCommandHandler(IBitBayPayFacade bitBayPayFacade)
+        public PayMembershipsFeeCommandHandler(IBitBayPayFacade bitBayPayFacade, IUserAccountDataRepository userAccountDataRepository)
         {
             _bitBayPayFacade = bitBayPayFacade;
+            _userAccountDataRepository = userAccountDataRepository;
         }
 
-        public Task<Unit> Handle(PayMembershipsFeeCommand command, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<Unit> Handle(PayMembershipsFeeCommand command, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var orderId = Guid.NewGuid();
+            await ValidateCommand(command);
 
             //TODO: Tmp store orderId in DB with UserAccountDataId
+            var orderId = Guid.NewGuid();
 
-            _bitBayPayFacade.CreatePayment(orderId, command.Amount);
+            await _bitBayPayFacade.CreatePayment(orderId, command.Amount);
 
             //TODO: Logic after payment call
 
             throw new NotImplementedException();
+        }
+
+        private async Task ValidateCommand(PayMembershipsFeeCommand command)
+        {
+            var user = await _userAccountDataRepository.GetAsync(command.UserAccountDataId);
+            if (user is null)
+            {
+                throw new ValidationException("User with given ID not found");
+            }
         }
     }
 }
