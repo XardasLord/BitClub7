6469785aed5aa85413dbd9db3e3e03fa55677548
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-24 19:25:50 +0200

    Created event and event handler for the notification from the bitbaypay system directly about changing the payment's status

---
+++ BC7.Business.Implementation/Payments/Events/PaymentStatusChangedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+
+namespace BC7.Business.Implementation.Payments.Events
+{
+    public class PaymentStatusChangedEvent : INotification
+    {
+        public Guid PaymentId { get; set; }
+        public Guid OrderId { get; set; }
+        public double AmountToPayInSourceCurrency { get; set; }
+        public double AmountToPayInDestinationCurrency { get; set; }
+        public string Status { get; set; } // PAID / COMPLETED
+        public double PaidAmount { get; set; }
+    }
+}
---
+++ BC7.Business.Implementation/Payments/Events/PaymentStatusChangedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BC7.Business.Implementation.Payments.Events
+{
+    public class PaymentStatusChangedEventHandler :INotificationHandler<PaymentStatusChangedEvent>
+    {
+        public Task Handle(PaymentStatusChangedEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
