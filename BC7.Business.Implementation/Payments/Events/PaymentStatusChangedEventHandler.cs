29bb8842c76ba72c30c630802e5e78c3eaadfea6
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-26 20:12:13 +0200

    Event handler logic - done

--- BC7.Business.Implementation/Payments/Events/PaymentStatusChangedEventHandler.cs
+++ BC7.Business.Implementation/Payments/Events/PaymentStatusChangedEventHandler.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using BC7.Repository;
 using MediatR;
 
 namespace BC7.Business.Implementation.Payments.Events
 {
-    public class PaymentStatusChangedEventHandler :INotificationHandler<PaymentStatusChangedEvent>
+    public class PaymentStatusChangedEventHandler : INotificationHandler<PaymentStatusChangedEvent>
     {
-        public Task Handle(PaymentStatusChangedEvent notification, CancellationToken cancellationToken)
+        private readonly IPaymentHistoryRepository _paymentHistoryRepository;
+
+        public PaymentStatusChangedEventHandler(IPaymentHistoryRepository paymentHistoryRepository)
         {
-            throw new NotImplementedException();
+            _paymentHistoryRepository = paymentHistoryRepository;
+        }
+
+        public async Task Handle(PaymentStatusChangedEvent notification, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var paymentHistory = await _paymentHistoryRepository.GetAsync(notification.PaymentId);
+
+            paymentHistory.ChangeStatus(notification.Status);
+            paymentHistory.Paid(notification.PaidAmount);
+
+            await _paymentHistoryRepository.UpdateAsync(paymentHistory);
         }
     }
 }
