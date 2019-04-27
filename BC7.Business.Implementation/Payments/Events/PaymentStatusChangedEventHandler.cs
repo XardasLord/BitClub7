8e799d9fd29358e2af0f8893e017d0a999a9a0fd
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-27 18:39:52 +0200

    In the event PaymentStatusChanged added notifing proper payment change based on a PaymentFor value (for now it's only the Memberships Fee flag update)

--- BC7.Business.Implementation/Payments/Events/PaymentStatusChangedEventHandler.cs
+++ BC7.Business.Implementation/Payments/Events/PaymentStatusChangedEventHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using BC7.Infrastructure.CustomExceptions;
 using BC7.Repository;
 using MediatR;
 
@@ -7,10 +9,12 @@ namespace BC7.Business.Implementation.Payments.Events
 {
     public class PaymentStatusChangedEventHandler : INotificationHandler<PaymentStatusChangedEvent>
     {
+        private readonly IUserAccountDataRepository _userAccountDataRepository;
         private readonly IPaymentHistoryRepository _paymentHistoryRepository;
 
-        public PaymentStatusChangedEventHandler(IPaymentHistoryRepository paymentHistoryRepository)
+        public PaymentStatusChangedEventHandler(IPaymentHistoryRepository paymentHistoryRepository, IUserAccountDataRepository userAccountDataRepository)
         {
+            _userAccountDataRepository = userAccountDataRepository;
             _paymentHistoryRepository = paymentHistoryRepository;
         }
 
@@ -22,6 +26,31 @@ namespace BC7.Business.Implementation.Payments.Events
             paymentHistory.Paid(notification.PaidAmount);
 
             await _paymentHistoryRepository.UpdateAsync(paymentHistory);
+            await NotifyProperPaymentChange(notification.OrderId, paymentHistory.PaymentFor);
+        }
+
+        private Task NotifyProperPaymentChange(Guid orderId, string paymentFor)
+        {
+            switch (paymentFor)
+            {
+                case "MembershipsFee": // TODO: Maybe it should be enum for the simplicity? If yes then migration is necessary to change the PaymentFor table in domain
+                    return MembershipsFeePaid(orderId);
+                //TODO: Other types in the future
+            }
+
+            throw new ValidationException($"Unknown paymentFor value: {paymentFor}");
+        }
+
+        private async Task MembershipsFeePaid(Guid orderId)
+        {
+            var userAccount = await _userAccountDataRepository.GetAsync(orderId);
+            if (userAccount is null)
+            {
+                throw new ValidationException($"Cannot find the UserAccountData from PaymentHistory with OrderId: {orderId}");
+            }
+
+            userAccount.PaidMembershipFee();
+            await _userAccountDataRepository.UpdateAsync(userAccount);
         }
     }
 }
