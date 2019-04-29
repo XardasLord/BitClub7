b407ea02a488e736b3f190f11ad4f9503f9b0c63
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-29 15:59:49 +0200

    Created steps which are needed for prevalidation action (using comments)

--- BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
+++ BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
@@ -13,6 +13,11 @@ namespace BC7.Business.Implementation.MatrixPositions.Commands.UpgradeMatrix
 
         public Task<Guid> Handle(UpgradeMatrixCommand request, CancellationToken cancellationToken)
         {
+            // 1. Sprawdzenie czy ID multikonta istnieje
+            // 2. Sprawdzenie czy użytkownik posiada wykupione miejsce w matrycy na levelu niżej
+            // 3. Sprzedzenie czy użytkownik opłacił miejsce w matrycy, do której chce zrobić upgrade
+            // 4. Sprawdzenie czy admin (znajdujący się na samej górze matrycy o level niższej) ma wykupione już miejsce w matrycy, do której chcemy zrobić upgrade
+
             throw new NotImplementedException();
         }
     }
