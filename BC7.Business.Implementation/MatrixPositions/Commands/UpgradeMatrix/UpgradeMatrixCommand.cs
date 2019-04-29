41200f0d4ceff687044a0c90e242cfca5da41a3e
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-29 15:50:51 +0200

    Created endpoint for upgrading the matrix level + created empty command and its handler

---
+++ BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace BC7.Business.Implementation.MatrixPositions.Commands.UpgradeMatrix
+{
+    public class UpgradeMatrixCommand : IRequest<Guid>
+    {
+        public int MatrixLevel { get; set; }
+        public Guid UserMultiAccountId { get; set; }
+    }
+}
---
+++ BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BC7.Business.Implementation.MatrixPositions.Commands.UpgradeMatrix
+{
+    public class UpgradeMatrixCommandHandler : IRequestHandler<UpgradeMatrixCommand, Guid>
+    {
+        public UpgradeMatrixCommandHandler()
+        {
+        }
+
+        public Task<Guid> Handle(UpgradeMatrixCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
