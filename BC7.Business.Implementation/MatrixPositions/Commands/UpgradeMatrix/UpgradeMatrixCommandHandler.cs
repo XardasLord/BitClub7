bc546a9ef5ca9dde71374a544ebb96ec73f13736
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-04-30 06:26:18 +0200

    Second prevalidation in command

--- BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
+++ BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
@@ -10,10 +10,12 @@ namespace BC7.Business.Implementation.MatrixPositions.Commands.UpgradeMatrix
     public class UpgradeMatrixCommandHandler : IRequestHandler<UpgradeMatrixCommand, Guid>
     {
         private readonly IUserMultiAccountRepository _userMultiAccountRepository;
+        private readonly IMatrixPositionRepository _matrixPositionRepository;
 
-        public UpgradeMatrixCommandHandler(IUserMultiAccountRepository userMultiAccountRepository)
+        public UpgradeMatrixCommandHandler(IUserMultiAccountRepository userMultiAccountRepository, IMatrixPositionRepository matrixPositionRepository)
         {
             _userMultiAccountRepository = userMultiAccountRepository;
+            _matrixPositionRepository = matrixPositionRepository;
         }
 
         public async Task<Guid> Handle(UpgradeMatrixCommand command, CancellationToken cancellationToken = default(CancellationToken))
@@ -26,6 +28,12 @@ namespace BC7.Business.Implementation.MatrixPositions.Commands.UpgradeMatrix
             }
 
             // 2. Sprawdzenie czy użytkownik posiada wykupione miejsce w matrycy na levelu niżej
+            var lowerLevelMatrix = command.MatrixLevel - 1;
+            if (await _matrixPositionRepository.GetMatrixForGivenMultiAccountAsync(user.Id, lowerLevelMatrix) is null)
+            {
+                throw new ValidationException($"User does not have position in the lower level matrix: {lowerLevelMatrix}");
+            }
+
             // 3. Sprzedzenie czy użytkownik opłacił miejsce w matrycy, do której chce zrobić upgrade
             // 4. Sprawdzenie czy admin (znajdujący się na samej górze matrycy o level niższej) ma wykupione już miejsce w matrycy, do której chcemy zrobić upgrade
 
