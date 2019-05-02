0b56ecf78891d8136ec538e2e7c298680d59c35d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-02 12:21:31 +0200

    Created command for buying matrix entrance for the specific level

---
+++ BC7.Business.Implementation/Payments/Commands/PayMatrixLevel/PayMatrixLevelCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BC7.Business.Implementation.Payments.Commands.PayMatrixLevel
+{
+    public class PayMatrixLevelCommandHandler : IRequestHandler<PayMatrixLevelCommand, string>
+    {
+        public PayMatrixLevelCommandHandler()
+        {
+        }
+
+        public Task<string> Handle(PayMatrixLevelCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
