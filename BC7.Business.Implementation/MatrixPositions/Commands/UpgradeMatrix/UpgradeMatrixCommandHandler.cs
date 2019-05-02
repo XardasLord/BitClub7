f493b4d99870c888076cb470d0028dc75a30d9df
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-02 16:59:15 +0200

    Used helper instead of logic check inside the handler

--- BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
+++ BC7.Business.Implementation/MatrixPositions/Commands/UpgradeMatrix/UpgradeMatrixCommandHandler.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using BC7.Domain;
+using BC7.Business.Helpers;
 using BC7.Infrastructure.CustomExceptions;
 using BC7.Repository;
 using MediatR;
@@ -13,43 +12,40 @@ namespace BC7.Business.Implementation.MatrixPositions.Commands.UpgradeMatrix
     {
         private readonly IUserMultiAccountRepository _userMultiAccountRepository;
         private readonly IMatrixPositionRepository _matrixPositionRepository;
-        private readonly IPaymentHistoryRepository _paymentHistoryRepository;
+        private readonly IPaymentHistoryHelper _paymentHistoryHelper;
 
         public UpgradeMatrixCommandHandler(
             IUserMultiAccountRepository userMultiAccountRepository,
             IMatrixPositionRepository matrixPositionRepository,
-            IPaymentHistoryRepository paymentHistoryRepository)
+            IPaymentHistoryHelper paymentHistoryHelper)
         {
             _userMultiAccountRepository = userMultiAccountRepository;
             _matrixPositionRepository = matrixPositionRepository;
-            _paymentHistoryRepository = paymentHistoryRepository;
+            _paymentHistoryHelper = paymentHistoryHelper;
         }
 
         public async Task<Guid> Handle(UpgradeMatrixCommand command, CancellationToken cancellationToken = default(CancellationToken))
         {
-            // 1. Sprawdzenie czy ID multikonta istnieje
-            var user = await _userMultiAccountRepository.GetAsync(command.UserMultiAccountId);
-            if (user is null)
+            var multiAccount = await _userMultiAccountRepository.GetAsync(command.UserMultiAccountId);
+            if (multiAccount is null)
             {
                 throw new ValidationException("User with given ID was not found");
             }
-
-            // 2. Sprawdzenie czy użytkownik posiada wykupione miejsce w matrycy na levelu niżej
+            
             var lowerLevelMatrix = command.MatrixLevel - 1;
-            if (await _matrixPositionRepository.GetMatrixForGivenMultiAccountAsync(user.Id, lowerLevelMatrix) is null)
+            if (await _matrixPositionRepository.GetMatrixForGivenMultiAccountAsync(multiAccount.Id, lowerLevelMatrix) is null)
             {
                 throw new ValidationException($"User does not have position in the lower level matrix: {lowerLevelMatrix}");
             }
-
-            // 3. Sprawdzenie czy użytkownik opłacił miejsce w matrycy, do której chce zrobić upgrade
-            var userPayments = await _paymentHistoryRepository.GetPaymentsByUser(user.Id);
-            //TODO: It shouldn't be PaymentForHelper.MatrixLevel0Position - need to calculate which matrix is it. To Rethink...
-            if (userPayments.Any(x => x.PaymentFor == PaymentForHelper.MatrixLevelPositionsDictionary[command.MatrixLevel] && (x.Status == PaymentStatusHelper.Paid || x.Status == PaymentStatusHelper.Completed))) // TODO: Move to helper
+            
+            if (await _paymentHistoryHelper.DoesUserPaidForMatrixLevelAsync(command.MatrixLevel, multiAccount.Id) == false)
             {
                 throw new ValidationException("User didn't pay for the upgraded matrix yet");
             }
 
             // 4. Sprawdzenie czy admin (znajdujący się na samej górze matrycy o level niższej) ma wykupione już miejsce w matrycy, do której chcemy zrobić upgrade
+
+
             // TODO: REST LOGIC
 
             throw new NotImplementedException();
