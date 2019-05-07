309e4dfea79a90a4e60dd7db46c6e12954ecd145
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-07 18:51:58 +0200

    Created migration which adds the decimal precision for decimal columns in database

---
+++ BC7.Database/Migrations/20190507164010_AddedDecimalPrecision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BC7.Database.Migrations
+{
+    public partial class AddedDecimalPrecision : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
