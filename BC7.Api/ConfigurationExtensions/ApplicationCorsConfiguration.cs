23278f60ac0ce77c42939dd536f58a8eae303a14
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-08 21:57:06 +0200

    Added CORS configuration in API startup

---
+++ BC7.Api/ConfigurationExtensions/ApplicationCorsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BC7.Api.ConfigurationExtensions
+{
+    public static class ApplicationCorsConfiguration
+    {
+        public static IServiceCollection ConfigureApplicationCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
+
+            return services;
+        }
+    }
+}
--- BC7.Api/Startup.cs
+++ BC7.Api/Startup.cs
@@ -12,25 +12,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NLog.Web;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace BC7.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureApplicationDependencies(Configuration);
             services.ConfigureApplicationJwtAuthorization(Configuration);
+            services.ConfigureApplicationCors();
 
             services.AddAutoMapper();
 
