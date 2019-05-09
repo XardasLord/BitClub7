3f8f6c0843e4ffdf3dd9b755ff3ca4e0dbc328cd
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-09 18:43:47 +0200

    Created helper for generating the matrix tree structure for the specific level. It uses the Graphviz library

---
+++ BC7.Business.Implementation/MatrixPositions/Requests/GenerateTreeDefinitionFile/GenerateTreeDefinitionFileRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BC7.Business.Implementation.MatrixPositions.Requests.GenerateTreeDefinitionFile
+{
+    public class GenerateTreeDefinitionFileRequest : IRequest
+    {
+        public int MatrixLevel { get; set; }
+    }
+}
---
+++ BC7.Business.Implementation/MatrixPositions/Requests/GenerateTreeDefinitionFile/GenerateTreeDefinitionFileRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using BC7.Business.Helpers;
+using MediatR;
+
+namespace BC7.Business.Implementation.MatrixPositions.Requests.GenerateTreeDefinitionFile
+{
+    public class GenerateTreeDefinitionFileRequestHandler : IRequestHandler<GenerateTreeDefinitionFileRequest>
+    {
+        private readonly IMatrixPositionHelper _matrixPositionHelper;
+
+        public GenerateTreeDefinitionFileRequestHandler(IMatrixPositionHelper matrixPositionHelper)
+        {
+            _matrixPositionHelper = matrixPositionHelper;
+        }
+
+        public async Task<Unit> Handle(GenerateTreeDefinitionFileRequest request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var fileDefinition = await _matrixPositionHelper.GenerateMatrixStructureTreeFileDefinition(request.MatrixLevel);
+            
+            using (var sw = new StreamWriter(@"c:\temp\matrixTreeDefinition.dot")) // TODO: Move the path to the configuration file
+            {
+                sw.WriteLine("digraph G {");
+                sw.Write(fileDefinition);
+                sw.WriteLine("}");
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
