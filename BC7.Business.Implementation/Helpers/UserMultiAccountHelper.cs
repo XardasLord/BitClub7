a386a672f74d11208eae798620a1fcca96462863
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-22 23:45:00 +0200

    Refactored helper

--- BC7.Business.Implementation/Helpers/UserMultiAccountHelper.cs
+++ BC7.Business.Implementation/Helpers/UserMultiAccountHelper.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using BC7.Business.Helpers;
 using BC7.Database;
 using BC7.Domain;
+using BC7.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace BC7.Business.Implementation.Helpers
@@ -11,10 +12,12 @@ namespace BC7.Business.Implementation.Helpers
     public class UserMultiAccountHelper : IUserMultiAccountHelper
     {
         private readonly IBitClub7Context _context;
+        private readonly IUserAccountDataRepository _userAccountDataRepository;
 
-        public UserMultiAccountHelper(IBitClub7Context context)
+        public UserMultiAccountHelper(IBitClub7Context context, IUserAccountDataRepository userAccountDataRepository)
         {
             _context = context;
+            _userAccountDataRepository = userAccountDataRepository;
         }
 
         public Task<UserMultiAccount> GetRandomUserMultiAccount()
@@ -27,13 +30,9 @@ namespace BC7.Business.Implementation.Helpers
 
         public async Task<string> GenerateNextMultiAccountName(Guid userAccountDataId)
         {
-            var userAccount = await _context.Set<UserAccountData>()
-                .Include(x => x.UserMultiAccounts)
-                .Where(x => x.Id == userAccountDataId)
-                .SingleAsync();
+            var userAccount = await _userAccountDataRepository.GetAsync(userAccountDataId);
 
             var numberOfMultiAccounts = userAccount.UserMultiAccounts.Count;
-
             if (numberOfMultiAccounts == 0)
             {
                 return userAccount.Login;
