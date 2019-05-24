0ae349c651ec6f4f2c63c69e9d070c585f8722ac
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-24 11:21:45 +0200

    Created unit test to check the Generating multi account name for new account for edge cases

---
+++ BC7.Business.Implementation.Tests.Unit/UserMultiAccountHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using BC7.Business.Implementation.Helpers;
+using BC7.Business.Implementation.Tests.Integration.FakerSeedGenerator;
+using BC7.Database;
+using BC7.Repository;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace BC7.Business.Implementation.Tests.Unit
+{
+    public class UserMultiAccountHelperTests
+    {
+        private readonly FakerGenerator _fakeGenerator;
+        private readonly Mock<IBitClub7Context> _contextMock;
+        private readonly Mock<IUserAccountDataRepository> _userAccountDataRepositoryMock;
+
+        public UserMultiAccountHelperTests()
+        {
+            _fakeGenerator = new FakerGenerator();
+            _contextMock = new Mock<IBitClub7Context>(MockBehavior.Strict);
+            _userAccountDataRepositoryMock = new Mock<IUserAccountDataRepository>(MockBehavior.Strict);
+        }
+
+        [Test]
+        [TestCase(0, "TestUser")]
+        [TestCase(1, "TestUser-001")]
+        [TestCase(5, "TestUser-005")]
+        public async Task GenerateNextMultiAccountName_WhenCalled_ReturnsCorrectMultiAccountName(int numberOfMultiAccounts, string expectedResult)
+        {
+            var sut = new UserMultiAccountHelper(_contextMock.Object, _userAccountDataRepositoryMock.Object);
+            var userAccountDataId = Guid.NewGuid();
+            var multiAccounts = _fakeGenerator.GetUserMultiAccountFakerGenerator().Generate(numberOfMultiAccounts);
+
+            _userAccountDataRepositoryMock.Setup(x => x.GetAsync(userAccountDataId))
+                .Returns(
+                    Task.FromResult(_fakeGenerator.GetUserAccountDataFakerGenerator()
+                        .RuleFor(x => x.Login, "TestUser")
+                        .RuleFor(x => x.UserMultiAccounts, multiAccounts)
+                        .Generate()));
+
+            var result = await sut.GenerateNextMultiAccountName(userAccountDataId);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
