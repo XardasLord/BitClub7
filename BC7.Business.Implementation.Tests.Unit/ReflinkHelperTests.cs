26c990f47ff63f6415c502fd661ff0952a60beb8
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-24 10:35:35 +0200

    Created unit test to check GenerateReflink() method

---
+++ BC7.Business.Implementation.Tests.Unit/ReflinkHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BC7.Business.Implementation.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BC7.Business.Implementation.Tests.Unit
+{
+    public class ReflinkHelperTests
+    {
+        private readonly ReflinkHelper _reflinkHelper;
+
+        public ReflinkHelperTests()
+        {
+            _reflinkHelper = new ReflinkHelper();
+        }
+
+        [Test]
+        public void GenerateReflink_WhenCalled_ReturnsDifferentHashEachTime()
+        {
+            // Arrange
+            var hashes = new List<string>();
+
+            // Act
+            for (var i = 0; i < 10; i++)
+            {
+                hashes.Add(_reflinkHelper.GenerateReflink());
+            }
+
+            // Assert
+            hashes.Should().OnlyHaveUniqueItems();
+        }
+    }
+}
