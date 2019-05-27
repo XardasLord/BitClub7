f33c33d9c7742781f48fe6092675438e50efa3a0
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-27 10:58:41 +0200

    Created API endpoint + documentation to add new article

---
+++ BC7.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using BC7.Business.Implementation.Articles.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BC7.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ArticlesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Create new article available for root user only
+        /// </summary>
+        /// <param name="command">Command object contains user ID, title and text of the article</param>
+        /// <returns>Returns OK (200)</returns>
+        [HttpPost]
+        [Authorize(Roles = "Root")]
+        public async Task<IActionResult> CreateArticle(CreateArticleCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+    }
+}
