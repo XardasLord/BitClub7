4f0bc434221659dc205cbdeb1840369080eb5e6e
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-27 15:33:57 +0200

    Created an API endpoint to get all articles + swagger documentation

--- BC7.Api/Controllers/ArticlesController.cs
+++ BC7.Api/Controllers/ArticlesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using BC7.Business.Implementation.Articles.Commands;
+using BC7.Business.Implementation.Articles.Commands.CreateArticle;
+using BC7.Business.Implementation.Articles.Requests;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +18,18 @@ namespace BC7.Api.Controllers
         }
 
         /// <summary>
+        /// Get all articles
+        /// </summary>
+        /// <returns>Returns model with list of articles</returns>
+        /// <response code="200">Returns model with list of articles</response>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllArticles()
+        {
+            return Ok(await _mediator.Send(new GetArticlesRequest()));
+        }
+
+        /// <summary>
         /// Create new article available for root user only
         /// </summary>
         /// <param name="command">Command object contains user ID, title and text of the article</param>
@@ -25,6 +38,7 @@ namespace BC7.Api.Controllers
         [Authorize(Roles = "Root")]
         public async Task<IActionResult> CreateArticle(CreateArticleCommand command)
         {
+            // TODO: 201 maybe?
             await _mediator.Send(command);
             return Ok();
         }
