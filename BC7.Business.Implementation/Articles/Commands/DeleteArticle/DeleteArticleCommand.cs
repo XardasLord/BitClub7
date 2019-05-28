b73dc47b6f95f52ff96ce2b153d5533b60b44aa5
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-28 11:12:40 +0200

    Command + handler to delete article from DB

---
+++ BC7.Business.Implementation/Articles/Commands/DeleteArticle/DeleteArticleCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace BC7.Business.Implementation.Articles.Commands.DeleteArticle
+{
+    public class DeleteArticleCommand : IRequest
+    {
+        public Guid ArticleId { get; set; }
+    }
+}
---
+++ BC7.Business.Implementation/Articles/Commands/DeleteArticle/DeleteArticleCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BC7.Repository;
+using MediatR;
+
+namespace BC7.Business.Implementation.Articles.Commands.DeleteArticle
+{
+    public class DeleteArticleCommandHandler : IRequestHandler<DeleteArticleCommand>
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public DeleteArticleCommandHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteArticleCommand command, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await _articleRepository.DeleteAsync(command.ArticleId);
+
+            return Unit.Value;
+        }
+    }
+}
