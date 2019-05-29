cff9ab8e7347b1275d7f3ef6635409a4b5e2b2cd
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-29 14:45:55 +0200

    Created ticket repository with creating new one

---
+++ BC7.Repository.Implementation/TicketRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using BC7.Database;
+using BC7.Domain;
+
+namespace BC7.Repository.Implementation
+{
+    public class TicketRepository : ITicketRepository
+    {
+        private readonly IBitClub7Context _context;
+
+        public TicketRepository(IBitClub7Context context)
+        {
+            _context = context;
+        }
+
+        public Task CreateAsync(Ticket ticket)
+        {
+            _context.Set<Ticket>().Add(ticket);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BC7.Repository/ITicketRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using BC7.Domain;
+
+namespace BC7.Repository
+{
+    public interface ITicketRepository
+    {
+        Task CreateAsync(Ticket ticket);
+    }
+}
