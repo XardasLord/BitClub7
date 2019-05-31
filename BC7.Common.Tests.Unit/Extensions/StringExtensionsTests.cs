b8633317c7d2d370cb604d0e01158b343adbde61
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-05-31 11:09:53 +0200

    Test to check the IsNullOrWhiteSpace extenstion method on strings works correctly

---
+++ BC7.Common.Tests.Unit/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using BC7.Common.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BC7.Common.Tests.Unit.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Test]
+        [TestCase(null, true)]
+        [TestCase("", true)]
+        [TestCase("a", false)]
+        [TestCase("example", false)]
+        public void IsNullOrWhiteSpace_WhenCalled_ReturnCorrectValue(string inputString, bool expectedValue)
+        {
+            var result = inputString.IsNullOrWhiteSpace();
+
+            result.Should().Be(expectedValue);
+        }
+    }
+}
