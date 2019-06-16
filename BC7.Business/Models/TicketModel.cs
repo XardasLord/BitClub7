0e9bf5785aacc60181d4050834d270a8a8d31327
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-06-16 17:16:44 +0200

    Added Id to TicketModel class

--- BC7.Business/Models/TicketModel.cs
+++ BC7.Business/Models/TicketModel.cs
@@ -1,7 +1,10 @@
-﻿namespace BC7.Business.Models
+﻿using System;
+
+namespace BC7.Business.Models
 {
     public class TicketModel
     {
+        public Guid Id { get; set; }
         public string TicketNumber { get; set; }
         public string SenderEmail { get; set; }
         public string Subject { get; set; }
