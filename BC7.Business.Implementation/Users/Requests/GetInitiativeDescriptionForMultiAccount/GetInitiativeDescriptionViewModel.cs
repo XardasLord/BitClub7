c0ebaf2195776f8b75aae7b4243aaf2bde9d3d09
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-07-21 15:28:53 +0200

    Added two ned fields on returning InitiativeDescription viewmodel - projectID (login), project code (reflink)

--- BC7.Business.Implementation/Users/Requests/GetInitiativeDescriptionForMultiAccount/GetInitiativeDescriptionViewModel.cs
+++ BC7.Business.Implementation/Users/Requests/GetInitiativeDescriptionForMultiAccount/GetInitiativeDescriptionViewModel.cs
@@ -3,5 +3,15 @@
     public class GetInitiativeDescriptionViewModel
     {
         public string Initiative { get; set; }
+
+        /// <summary>
+        /// Login
+        /// </summary>
+        public string ProjectId { get; set; }
+
+        /// <summary>
+        /// Reflink
+        /// </summary>
+        public string ProjectCode { get; set; }
     }
 }
