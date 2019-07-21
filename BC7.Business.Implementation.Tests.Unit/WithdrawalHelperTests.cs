b9abb9f44ca2a3d316db4fb0890b1b811b2f34c9
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-07-21 15:53:07 +0200

    Created unit tests to veryfi the withdrawal calculator helper

---
+++ BC7.Business.Implementation.Tests.Unit/WithdrawalHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using BC7.Business.Implementation.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BC7.Business.Implementation.Tests.Unit
+{
+    public class WithdrawalHelperTests
+    {
+        private readonly WithdrawalHelper _withdrawalHelper;
+
+        public WithdrawalHelperTests()
+        {
+            _withdrawalHelper = new WithdrawalHelper();
+        }
+
+        public decimal Act(int matrixLevel)
+        {
+            return _withdrawalHelper.CalculateAmountToWithdraw(matrixLevel);
+        }
+
+        [Theory]
+        [TestCase(0, 0.00905)]
+        [TestCase(1, 0.02715)]
+        [TestCase(2, 0.0543)]
+        [TestCase(3, 0.1086)]
+        [TestCase(4, 0.22625)]
+        [TestCase(5, 0.4525)]
+        [TestCase(6, 0.905)]
+        public void CalculateAmountToWithdraw_WhenCalled_ReturnsCorrectCalculatedValue(int matrixLevel, decimal expectedResult)
+        {
+            var result = Act(matrixLevel);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
--- BC7.Business.Implementation/Helpers/WithdrawalHelper.cs
+++ BC7.Business.Implementation/Helpers/WithdrawalHelper.cs
@@ -5,33 +5,34 @@ namespace BC7.Business.Implementation.Helpers
 {
     public class WithdrawalHelper : IWithdrawalHelper
     {
+        private const decimal Lvl0 = 0.01M;
+        private const decimal Lvl1 = 0.03M;
+        private const decimal Lvl2 = 0.06M;
+        private const decimal Lvl3 = 0.12M;
+        private const decimal Lvl4 = 0.25M;
+        private const decimal Lvl5 = 0.50M;
+        private const decimal Lvl6 = 1.00M;
+        private const decimal AdminFee = 0.095M;
+
         public decimal CalculateAmountToWithdraw(int matrixLevel)
         {
-            var lvl0 = 0.01M;
-            var lvl1 = 0.03M;
-            var lvl2 = 0.06M;
-            var lvl3 = 0.12M;
-            var lvl4 = 0.25M;
-            var lvl5 = 0.50M;
-            var lvl6 = 1.00M;
-            var adminFee = 0.095M;
 
             switch (matrixLevel)
             {
                 case 0:
-                    return lvl0 - (adminFee * lvl0);
-                case 1:
-                    return lvl1 - (adminFee * lvl1);
-                case 2:
-                    return lvl2 - (adminFee * lvl2);
-                case 3:
-                    return lvl3 - (adminFee * lvl3);
-                case 4:
-                    return lvl4 - (adminFee * lvl4);
-                case 5:
-                    return lvl5 - (adminFee * lvl5);
-                case 6:
-                    return lvl6 - (adminFee * lvl6);
+                    return Lvl0 - (AdminFee * Lvl0);
+                case 1:                       
+                    return Lvl1 - (AdminFee * Lvl1);
+                case 2:                       
+                    return Lvl2 - (AdminFee * Lvl2);
+                case 3:                       
+                    return Lvl3 - (AdminFee * Lvl3);
+                case 4:                       
+                    return Lvl4 - (AdminFee * Lvl4);
+                case 5:                       
+                    return Lvl5 - (AdminFee * Lvl5);
+                case 6:                       
+                    return Lvl6 - (AdminFee * Lvl6);
                 default:
                     throw new ArgumentOutOfRangeException($"Invalid matrixLevel value: {matrixLevel}");
             }
