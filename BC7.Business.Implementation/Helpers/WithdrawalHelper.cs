15b558a49ef191db2a2688587f0c81114e857f76
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-07-26 21:59:28 +0200

    Deleted admin fee on withdrawals from matrix

--- BC7.Business.Implementation/Helpers/WithdrawalHelper.cs
+++ BC7.Business.Implementation/Helpers/WithdrawalHelper.cs
@@ -20,19 +20,19 @@ namespace BC7.Business.Implementation.Helpers
             switch (matrixLevel)
             {
                 case 0:
-                    return Lvl0 - (AdminFee * Lvl0);
-                case 1:                       
-                    return Lvl1 - (AdminFee * Lvl1);
+                    return Lvl0;
+                case 1:
+                    return Lvl1;
                 case 2:                       
-                    return Lvl2 - (AdminFee * Lvl2);
+                    return Lvl2;
                 case 3:                       
-                    return Lvl3 - (AdminFee * Lvl3);
+                    return Lvl3;
                 case 4:                       
-                    return Lvl4 - (AdminFee * Lvl4);
+                    return Lvl4;
                 case 5:                       
-                    return Lvl5 - (AdminFee * Lvl5);
+                    return Lvl5;
                 case 6:                       
-                    return Lvl6 - (AdminFee * Lvl6);
+                    return Lvl6;
                 default:
                     throw new ArgumentOutOfRangeException($"Invalid matrixLevel value: {matrixLevel}");
             }
@@ -40,7 +40,7 @@ namespace BC7.Business.Implementation.Helpers
 
         public decimal CalculateAmountToWithdraw(decimal amount)
         {
-            return amount - (AdminFee * amount);
+            return amount;
         }
     }
 }
