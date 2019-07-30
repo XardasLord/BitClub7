b21392f331b52dc1b790f4b6dabd30cdb012b274
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-07-30 19:53:32 +0200

    Fixed updating email

--- BC7.Business/Models/UpdateUserModel.cs
+++ BC7.Business/Models/UpdateUserModel.cs
@@ -2,6 +2,7 @@
 {
     public class UpdateUserModel
     {
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Street { get; set; }
