5c997c22139e084b52ab7142443f3a7ed4c42d30
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-08-01 16:01:35 +0200

    Added more detailed informations about user to home page

--- BC7.Business.Implementation/Users/Requests/GetUser/GetUserRequestHandler.cs
+++ BC7.Business.Implementation/Users/Requests/GetUser/GetUserRequestHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using BC7.Business.Helpers;
 using BC7.Business.Models;
 using BC7.Infrastructure.CustomExceptions;
 using BC7.Repository;
@@ -12,11 +14,19 @@ namespace BC7.Business.Implementation.Users.Requests.GetUser
     public class GetUserRequestHandler : IRequestHandler<GetUserRequest, UserAccountDataModel>
     {
         private readonly IUserAccountDataRepository _userAccountDataRepository;
+        private readonly IWithdrawalRepository _withdrawalRepository;
+        private readonly IUserMultiAccountHelper _multiAccountHelper;
         private readonly IMapper _mapper;
 
-        public GetUserRequestHandler(IUserAccountDataRepository userAccountDataRepository, IMapper mapper)
+        public GetUserRequestHandler(
+            IUserAccountDataRepository userAccountDataRepository, 
+            IWithdrawalRepository withdrawalRepository, 
+            IUserMultiAccountHelper multiAccountHelper,
+            IMapper mapper)
         {
             _userAccountDataRepository = userAccountDataRepository;
+            _withdrawalRepository = withdrawalRepository;
+            _multiAccountHelper = multiAccountHelper;
             _mapper = mapper;
         }
 
@@ -27,6 +37,14 @@ namespace BC7.Business.Implementation.Users.Requests.GetUser
             var user = await _userAccountDataRepository.GetAsync(request.UserId);
 
             var userModel = _mapper.Map<UserAccountDataModel>(user);
+
+            // More details for the home page
+            var userWithdrawals = await _withdrawalRepository.GetAllAsync(user.UserMultiAccounts.Select(x => x.Id));
+            var accountsWhereRequestedUserIsSponsor = await _multiAccountHelper.GetAllWhereMultiAccountsAreSponsors(user.UserMultiAccounts.Select(x => x.Id));
+
+            userModel.EarnedBtc = userWithdrawals.Sum(x => x.Amount);
+            userModel.InvitedAccountsTotalCount = accountsWhereRequestedUserIsSponsor.Count;
+            userModel.AccountsInMatrixTotalCount = user.UserMultiAccounts.Count(x => x.RefLink != null);
 
             return userModel;
         }
