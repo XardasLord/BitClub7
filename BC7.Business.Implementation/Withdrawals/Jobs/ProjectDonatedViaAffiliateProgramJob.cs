03a5ef480430b7604e69dc1646c5e313cd875f60
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-08-03 19:32:41 +0200

    Job refactoring

--- BC7.Business.Implementation/Withdrawals/Jobs/ProjectDonatedViaAffiliateProgramJob.cs
+++ BC7.Business.Implementation/Withdrawals/Jobs/ProjectDonatedViaAffiliateProgramJob.cs
@@ -31,72 +31,63 @@ namespace BC7.Business.Implementation.Withdrawals.Jobs
             
             // BC7Fee (9.5%)
             var bc7Fee = _withdrawalHelper.CalculateAmountToWithdraw(model.Amount, AffiliateProgramType.Bc7ConstFee);
-            var bc7ConstWithdrawal = new Withdrawal(
-                Guid.NewGuid(),
-                _rootId,
-                bc7Fee,
-                PaymentSystemType.BitBayPay,
-                WithdrawalForHelper.ProjectDonationViaAffiliateProgramBc7ConstFee);
 
-            BackgroundJob.Enqueue<CommitWithdrawalJob>(
-                job => job.Execute(bc7ConstWithdrawal, null));
+            ScheduleWithdrawalJob(
+                _rootId, 
+                bc7Fee, 
+                WithdrawalForHelper.ProjectDonationViaAffiliateProgramBc7ConstFee);
             
             var amountAfterTakenFee = model.Amount - bc7Fee;
 
 
             // DonatedDirectly (80%)
-            var directDonateWithdrawal = new Withdrawal(
-                Guid.NewGuid(),
-                model.DonatedUserMultiAccountId,
+            ScheduleWithdrawalJob(
+                model.DonatedUserMultiAccountId, 
                 _withdrawalHelper.CalculateAmountToWithdraw(amountAfterTakenFee, AffiliateProgramType.DirectDonate),
-                PaymentSystemType.BitBayPay,
                 WithdrawalForHelper.ProjectDonationViaAffiliateProgramDirectDonate);
-
-            BackgroundJob.Enqueue<CommitWithdrawalJob>(
-                job => job.Execute(directDonateWithdrawal, null));
 
 
             // DonatedLineA (10%)
             var lineAMultiAccount = await _userMultiAccountHelper.GetSponsorForMultiAccount(model.DonatedUserMultiAccountId);
-            var lineADonateWithdrawal = new Withdrawal(
-                Guid.NewGuid(),
+
+            ScheduleWithdrawalJob(
                 lineAMultiAccount.Id,
                 _withdrawalHelper.CalculateAmountToWithdraw(amountAfterTakenFee, AffiliateProgramType.AffiliateLineA),
-                PaymentSystemType.BitBayPay,
                 WithdrawalForHelper.ProjectDonationViaAffiliateProgramLineA);
-
-            BackgroundJob.Enqueue<CommitWithdrawalJob>(
-                job => job.Execute(lineADonateWithdrawal, null));
 
 
             // DonatedLineB (5%)
             var lineBMultiAccount = await _userMultiAccountHelper.GetSponsorForMultiAccount(lineAMultiAccount.Id);
-            var lineBDonateWithdrawal = new Withdrawal(
-                Guid.NewGuid(),
+
+            ScheduleWithdrawalJob(
                 lineBMultiAccount.Id,
                 _withdrawalHelper.CalculateAmountToWithdraw(amountAfterTakenFee, AffiliateProgramType.AffiliateLineB),
-                PaymentSystemType.BitBayPay,
                 WithdrawalForHelper.ProjectDonationViaAffiliateProgramLineB);
-
-            BackgroundJob.Enqueue<CommitWithdrawalJob>(
-                job => job.Execute(lineBDonateWithdrawal, null));
 
 
             // BC7DonatedFee (5%)
-            var bc7DonateFeeWithdrawal = new Withdrawal(
-                Guid.NewGuid(),
+            ScheduleWithdrawalJob(
                 _rootId,
                 _withdrawalHelper.CalculateAmountToWithdraw(amountAfterTakenFee, AffiliateProgramType.Bc7DonateFee),
-                PaymentSystemType.BitBayPay,
                 WithdrawalForHelper.ProjectDonationViaAffiliateProgramBc7DonateFee);
-
-            BackgroundJob.Enqueue<CommitWithdrawalJob>(
-                job => job.Execute(bc7DonateFeeWithdrawal, null));
             
 
             // TODO: WithdrawJob in the future to process automatically withdrawal via payment system to the user's btc wallet
 
             context.WriteLine("ProjectDonatedViaAffiliateProgramJob completed");
+        }
+
+        private void ScheduleWithdrawalJob(Guid multiAccountId, decimal amount, string withdrawalDescription)
+        {
+            var withdrawal = new Withdrawal(
+                Guid.NewGuid(),
+                multiAccountId,
+                amount,
+                PaymentSystemType.BitBayPay,
+                withdrawalDescription);
+
+            BackgroundJob.Enqueue<CommitWithdrawalJob>(
+                job => job.Execute(withdrawal, null));
         }
     }
 }
